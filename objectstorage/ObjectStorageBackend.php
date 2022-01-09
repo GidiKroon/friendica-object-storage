87b8ac7e531d5236f22f322818d48efe61f945be
Gidi Kroon <Gidi@gidikroon.eu>
2022-01-09 19:55:33 +0100

    Add use of AWS SDK for S3
    GetObject/PutObject/DeleteObject can be done against an AWS S3 bucket.
    More error handling is needed as well as testing against compatible
    services.

--- objectstorage/ObjectStorageBackend.php
+++ objectstorage/ObjectStorageBackend.php
@@ -2,18 +2,42 @@
 
 namespace Friendica\Addon\objectstorage;
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
+use Aws\S3\Exception\S3Exception;
 use Exception;
 use Friendica\Core\Storage\Exception\ReferenceStorageException;
 use Friendica\Core\Storage\Exception\StorageException;
 use Friendica\Core\Storage\Capability\ICanWriteToStorage;
 use Friendica\Util\Strings;
 
+/**
+ * Storage backend for object storage, e.g. in AWS S3.
+ * 
+ * Notes:
+ * 
+ * The S3 SDK supports using streams for the content to and from
+ * the bucket, but the Friendica storage SPI doesn't. So
+ * the full object data is in memory.
+ * 
+ * The Friendica storage SPI doesn't support setting a
+ * custom url for stored objects, e.g. for using a cdn
+ * or for direct linking to the bucket object. Friendica
+ * will always read the object for you. This has the
+ * nice consequence that the bucket doesn't need to be
+ * public and that no public ACL or bucket policy needs
+ * to be set.
+ */
 class ObjectStorageBackend implements ICanWriteToStorage
 {
-	const NAME = 'Object Storage';
+	const NAME = 'ObjectStorage';
 
 	/** @var ObjectStorageBackendConfig */
 	private $storageConfig;
+	/** @var S3Client */
+	private $s3Client;
 
 	/**
 	 * ObjectStorageBackend constructor.
@@ -23,6 +47,7 @@ class ObjectStorageBackend implements ICanWriteToStorage
 	public function __construct(ObjectStorageBackendConfig $storageConfig)
 	{
 		$this->storageConfig = $storageConfig;
+		$this->s3Client = $this->createS3();
 	}
 
 	/**
@@ -30,11 +55,24 @@ class ObjectStorageBackend implements ICanWriteToStorage
 	 */
 	public function get(string $reference): string
 	{
-		/// @TODO check reference, get contents
-		throw new ReferenceStorageException(sprintf('%s storage failed to get the file %s, The file is invalid', self::NAME, $reference));
-		$result = '';
+		$params = [
+			'Bucket' => $this->storageConfig->getBucket(),
+			'Key' => $this->storageConfig->getPrefix() . $reference,
+		];
 
-		return $result;
+		try {
+			$result = $this->s3Client->headObject($params);
+		} catch (S3Exception $exception) {
+			throw new ReferenceStorageException(sprintf('%s storage failed to get the file %s, The file is invalid', self::NAME, $reference), $exception->getCode(), $exception);
+		}
+
+		try {
+			$result = $this->s3Client->getObject($params);
+		} catch (S3Exception $exception) {
+			throw new StorageException(sprintf('%s storage failed to load the data', self::NAME), $exception->getCode(), $exception);
+		}
+
+		return $result['Body'];
 	}
 
 	/**
@@ -50,7 +88,18 @@ class ObjectStorageBackend implements ICanWriteToStorage
 			}
 		}
 
-		/// @TODO store contents
+		$params = [
+			'ACL' => 'bucket-owner-full-control',
+			'Body' => $data,
+			'Bucket' => $this->storageConfig->getBucket(),
+			'Key' => $this->storageConfig->getPrefix() . $reference,
+		];
+
+		try {
+			$result = $this->s3Client->putObject($params);
+		} catch (S3Exception $exception) {
+			throw new StorageException(sprintf('%s storage failed to store the data', self::NAME), $exception->getCode(), $exception);
+		}
 
 		return $reference;
 	}
@@ -60,6 +109,22 @@ class ObjectStorageBackend implements ICanWriteToStorage
 	 */
 	public function delete(string $reference)
 	{
+		$params = [
+			'Bucket' => $this->storageConfig->getBucket(),
+			'Key' => $this->storageConfig->getPrefix() . $reference,
+		];
+
+		try {
+			$result = $this->s3Client->headObject($params);
+		} catch (S3Exception $exception) {
+			throw new ReferenceStorageException(sprintf('%s storage failed to delete the file %s, The file is invalid', self::NAME, $reference), $exception->getCode(), $exception);
+		}
+
+		try {
+			$result = $this->s3Client->deleteObject($params);
+		} catch (S3Exception $exception) {
+			throw new StorageException(sprintf('%s storage failed to delete the data', self::NAME), $exception->getCode(), $exception);
+		}
 	}
 
 	/**
@@ -73,5 +138,21 @@ class ObjectStorageBackend implements ICanWriteToStorage
 	public function __toString(): string
 	{
 		return self::NAME;
+	}
+
+	private function createS3(): S3Client
+	{
+		$params = [
+			'version' => '2006-03-01',
+		];
+		if ($this->storageConfig->getRegion() != '') {
+			$params['region'] = $this->storageConfig->getRegion();
+		}
+		if ($this->storageConfig->getEndpoint() != '') {
+			$params['endpoint'] = $this->storageConfig->getEndpoint();
+		}
+
+		$s3Client = new S3Client($params);
+		return $s3Client;
 	}
 }
--- objectstorage/ObjectStorageBackendConfig.php
+++ objectstorage/ObjectStorageBackendConfig.php
@@ -6,6 +6,23 @@ use Friendica\Core\Config\Capability\IManageConfigValues;
 use Friendica\Core\L10n;
 use Friendica\Core\Storage\Capability\ICanConfigureStorage;
 
+/**
+ * Configuration class for ObjectStorageBackend. It enables a
+ * settings panel in the admin storage screens.
+ * 
+ * For AWS S3 one would normally set `objectstorage_region` and 
+ * `objectstorage_bucket`. For non-AWS S3 one would typically 
+ * set `objectstorage_endpoint` and `objectstorage_bucket`. The
+ * bucket path prefix would be useful if you use the bucket
+ * for other things as well, or if Friendica would allow different
+ * storage configurations for different uses (photos, attachments,
+ * avatars) which it currently doesn't.
+ * 
+ * The configuration does not include credentials. For AWS S3
+ * access should be given securely with an EC2 instance
+ * profile using an IAM role. Or credentials can be specified in
+ * a `~/.aws/credentials` file or in enviroment variables.
+ */
 class ObjectStorageBackendConfig implements ICanConfigureStorage
 {
 	/** @var IManageConfigValues */
@@ -13,7 +30,13 @@ class ObjectStorageBackendConfig implements ICanConfigureStorage
 	/** @var L10n */
 	private $l10n;
 	/** @var string */
+	private $region;
+	/** @var string */
 	private $endpoint;
+	/** @var string */
+	private $bucket;
+	/** @var string */
+	private $prefix;
 
 	/**
 	 * ObjectStorageBackendConfig constructor.
@@ -25,36 +48,103 @@ class ObjectStorageBackendConfig implements ICanConfigureStorage
 	{
 		$this->config = $config;
 		$this->l10n   = $l10n;
+		$this->region = $this->config->get('storage', 'objectstorage_region', '');
 		$this->endpoint = $this->config->get('storage', 'objectstorage_endpoint', '');
+		$this->bucket = $this->config->get('storage', 'objectstorage_bucket', '');
+		$this->prefix = $this->config->get('storage', 'objectstorage_prefix', '');
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function getOptions(): array
 	{
 		return [
+			'region' => [
+				'input',
+				$this->l10n->t('The AWS region'),
+				$this->region,
+				$this->l10n->t('Enter the AWS region, e.g. "eu-west-1". This is required for Amazon AWS S3.'),
+			],
 			'endpoint' => [
 				'input',
-				$this->l10n->t('The object storage endpoint'),
+				$this->l10n->t('The S3 endpoint'),
 				$this->endpoint,
-				$this->l10n->t('Enter the object storage endpoint, e.g. "eu-west-1.s3.amazonaws.com"'),
+				$this->l10n->t('Enter the S3 endpoint, e.g. "s3.eu-west-1.amazonaws.com". This is optional for Amazon AWS S3 but required for other S3 compatible services.'),
+			],
+			'bucket' => [
+				'input',
+				$this->l10n->t('The S3 bucket'),
+				$this->bucket,
+				$this->l10n->t('Enter the S3 bucket name, e.g. "my-bucket-example-org". This is required.'),
+			],
+			'prefix' => [
+				'input',
+				$this->l10n->t('The bucket path prefix'),
+				$this->prefix,
+				$this->l10n->t('Enter the path prefix to use for objects in the bucket, e.g. "photos/" or "friendica-". This is optional and defaults to "". It is useful if the bucket is used for other things as well.'),
 			],
 		];
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function saveOptions(array $data): array
 	{
+		$region = $data['region'] ?? '';
+		$this->config->set('storage', 'objectstorage_region', $region);
+		$this->region = $region;
+
 		$endpoint = $data['endpoint'] ?? '';
-		if ($endpoint === '') {
-			return [
-				'endpoint' => $this->l10n->t('Enter an endpoint')
-			];
-		};
 		$this->config->set('storage', 'objectstorage_endpoint', $endpoint);
 		$this->endpoint = $endpoint;
+
+		$bucket = $data['bucket'] ?? '';
+		if ($bucket === '') {
+			return [
+				'bucket' => $this->l10n->t('Enter a bucket name')
+			];
+		};
+		$this->config->set('storage', 'objectstorage_bucket', $bucket);
+		$this->bucket = $bucket;
+
+		$prefix = $data['prefix'] ?? '';
+		$this->config->set('storage', 'objectstorage_prefix', $prefix);
+		$this->prefix = $prefix;
+
 		return [];
 	}
 
+	/**
+	 * Get the AWS S3 region. Required for AWS S3.
+	 */
+	public function getRegion(): string
+	{
+		return $this->region;
+	}
+
+	/**
+	 * Get the S3-endpoint. Required for non-AWS S3.
+	 */
 	public function getEndpoint(): string
 	{
 		return $this->endpoint;
+	}
+
+	/**
+	 * Get the S3 bucket name. Required.
+	 */
+	public function getBucket(): string
+	{
+		return $this->bucket;
+	}
+
+	/**
+	 * Get the bucket prefix. Optional, defaults to ''. When used, typically ends with '/' or '-'.
+	 */
+	public function getPrefix(): string
+	{
+		return $this->prefix;
 	}
 }
